7e40133b87a067aaf08c25b9dabe06192bb65ce4
Dylan-Walters-Student <112421363+Dylan-Walters-Student@users.noreply.github.com>
2022-11-17 19:25:33 +0000

    Set up basic 2d array for map

--- Program.cs
+++ Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-       Console.WriteLine("Hi");
+        Map();
+    }
+    public static void Map()
+    {
+        Mapping mapUtility = new Mapping();
+        var mapArr = mapUtility.Array;
+        for (int i = 0; i < mapArr.GetLength(0); i++)
+        {
+            for (int j = 0; j <= mapArr.GetLength(0); j++)
+            {
+                if (j % 9 == 0)
+                {
+                    if ( j == 0) 
+                    {
+                        Console.Write(mapArr[i, j] + " ");
+                    }
+                    else
+                    {
+                        Console.WriteLine();
+                    }
+                }
+                else
+                {
+                    Console.Write(mapArr[i, j] + " ");
+                }
+            }
+        }
     }
 }
